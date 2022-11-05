96c299091b1483a1e7ec603b26d35a544141ccdc
TaiLe1101 <taic21a.th1@gmail.com>
2022-11-05 15:04:21 +0700

    update: catalog page

--- src/component/CardBtn/CardBtn.js
+++ src/component/CardBtn/CardBtn.js
@@ -1,19 +1,42 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import { image5 } from '~/utils/images';
 import styles from './CardBtn.module.scss';
 import Button from '~/component/Button';
 
 const cx = classNames.bind(styles);
 
-function CardBtn({ img, label, to }) {
+function CardBtn({ img, label, to, secondary = false, title }) {
   return (
-    <div className={cx('wrapper')}>
+    <div
+      className={cx('wrapper', {
+        secondary,
+      })}
+    >
       <img src={img} alt={label} />
-      <Button to={to} className={cx('btn')}>
-        {label}
-      </Button>
+
+      {secondary && (
+        <div className={cx('wrapper-content')}>
+          <h2 className={cx('title')}>{title}</h2>
+          <Button to={to} className={cx('btn')}>
+            {label}
+          </Button>
+        </div>
+      )}
+      {!secondary && (
+        <Button to={to} className={cx('btn')}>
+          {label}
+        </Button>
+      )}
     </div>
   );
 }
+
+CardBtn.propTypes = {
+  img: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+  center: PropTypes.bool,
+  title: PropTypes.string,
+};
 
 export default CardBtn;
--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,5 +1,8 @@
+import { Col, Row } from 'antd';
 import classNames from 'classnames/bind';
+import CardBtn from '~/component/CardBtn';
 import Search from '~/component/Search';
+import { image5 } from '~/utils/images';
 import styles from './Catalog.module.scss';
 import Section1 from './components/Section1';
 
@@ -11,6 +14,28 @@ function Catalog() {
       <div className="container">
         <Search></Search>
         <Section1></Section1>
+
+        <h2 className={cx('title')}> Nha khoa</h2>
+        <Row gutter={[32, 32]}>
+          <Col span={24} lg={{ span: 8 }}>
+            <CardBtn label="Tài Lê" img={image5} secondary title="Dụng cụ nha khoa"></CardBtn>
+          </Col>
+          <Col span={24} lg={{ span: 8 }}>
+            <CardBtn label="Tài Lê" img={image5} secondary title="Dụng cụ nha khoa"></CardBtn>
+          </Col>
+          <Col span={24} lg={{ span: 8 }}>
+            <CardBtn label="Tài Lê" img={image5} secondary title="Dụng cụ nha khoa"></CardBtn>
+          </Col>
+          <Col span={24} lg={{ span: 8 }}>
+            <CardBtn label="Tài Lê" img={image5} secondary title="Dụng cụ nha khoa"></CardBtn>
+          </Col>
+          <Col span={24} lg={{ span: 8 }}>
+            <CardBtn label="Tài Lê" img={image5} secondary title="Dụng cụ nha khoa"></CardBtn>
+          </Col>
+          <Col span={24} lg={{ span: 8 }}>
+            <CardBtn label="Tài Lê" img={image5} secondary title="Dụng cụ nha khoa"></CardBtn>
+          </Col>
+        </Row>
       </div>
     </div>
   );
